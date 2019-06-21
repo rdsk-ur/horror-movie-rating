a2dc83710283f86a81826e2ddca67f85b9b24dd2
cyd3r <cyd3rhackger@gmaill.com>
2019-06-21 11:29:30 +0200

    more cleanup, remove some plots, update readme

--- README.md
+++ README.md
@@ -1,13 +1,9 @@
 # Rating of Horror Movies
 
-This script tests the age old question if horror movies are on average rated worse than movies from the non-horror genres. This question was raised by me when browsing movie databases for flicks to watch. I often deemed the rating of horror movies to be unfair, while in other genres (e.g. comedy) the rating seemed "too good". The following code should answer that question.
+This script tests the age old question if horror movies are on average rated worse than movies from the non-horror genres. This question was raised when I was browsing the movie databases for flicks to watch. I often deemed the rating of horror movies to be unfair, while in other genres (e.g. comedy) the rating seemed to be too good. This script gives evidence that non-horror movies are in fact rated worse than other movies.
 
-## How to run
+## Prerequisites
 
-First, download the dataset from kaggle: https://www.kaggle.com/tmdb/tmdb-movie-metadata Move the CSV files to the same location as `movies.R`.
+The script is written in R and requires the `ggplot2` package.
 
-The script is written in R, the following packages are required:
-
-- ggplot2
-- dyplr
-- magrittr
+Download the dataset from kaggle: https://www.kaggle.com/tmdb/tmdb-movie-metadata Move the CSV files to the same location as `movies.R`.
--- movies.R
+++ movies.R
@@ -7,24 +7,17 @@ revenue<-movies$revenue
 rating<-movies$vote_average
 genres<-movies$genres
 
-##### plotting the features w/ histograms ###
-hist(budget)
-hist(revenue)
-hist(rating)
-
-##### correlation btw. features ###
+# correlation between features
 movieM<-t(rbind(budget, rating, revenue))
+cat("Correlation of budget, rating and revenue:\n")
 cor(movieM)
-cor(budget,revenue)
-plot(log(budget),log( revenue))
+plot(log(budget), log(revenue))
 
-##### comparing horror genre w/ non horror ###
+# select horror and non-horror movies
 horror<-movies[grepl("Horror",genres),]
 non_horror<-movies[!grepl("Horror",genres),]
-hist(horror$vote_average)
-hist(non_horror$vote_average)
-mean(horror$vote_average)
-mean(non_horror$vote_average)
+cat("Average rating of horror movies:", mean(horror$vote_average), "\n")
+cat("Average rating of non-horror movies:", mean(non_horror$vote_average), "\n")
 
 combine <- function(group1, group2, name1, name2) {
   return(data.frame(
@@ -33,49 +26,43 @@ combine <- function(group1, group2, name1, name2) {
   ))
 }
 
+# compare ratings of horror and non-horror
 df <- combine(horror$vote_average, non_horror$vote_average, "horror", "non_horror")
-
-##### groupwise plotting ###
 ggplot(df, aes(rating, fill = genregroup)) + 
   geom_histogram(alpha = 0.5, aes(y = ..density..), position = 'identity', bins = 20)+
   ggtitle(paste("avg rating for genregroup"))
 
-##### performing the welsh t test for significant differences ###
-#one sided because we want to test if horror movies are rated WORSE
+# performing the welsh t test for significant differences
+# one sided because we want to test if horror movies are rated WORSE
 hm.test <-t.test(horror$vote_average, non_horror$vote_average, alternative = "less")
-hm.test$p.value
+cat("t-test:", hm.test$p.value, "\n")
 
 # this suggests horror movies are indeed rated worse than non horror
 # my suspicion has been confirmed, 
 # but maybe that is because there are more low butget horror movies that fail in box office?
 # what happens if we only test the movies which generated a revenue over a certain threshold?
 
-##### comparing successfull horror genre w/ non-horror ###
-successful_horror<-horror[horror$revenue>1000000,]
-successful_non_horror<-non_horror[non_horror$revenue>1000000,]
-mean(successful_horror$vote_average)
-mean(successful_non_horror$vote_average)
+# compare ratings of successful horror and non-horror
+revenue_limit <- 1000000
+successful_horror <- horror[horror$revenue > revenue_limit,]
+successful_non_horror <- non_horror[non_horror$revenue > revenue_limit,]
+cat("Average rating of successful horror movies:", mean(successful_horror$vote_average), "\n")
+cat("Average rating of successful non-horror movies:", mean(successful_non_horror$vote_average), "\n")
 
 dh <- combine(successful_horror$vote_average, successful_non_horror$vote_average, "horror", "non_horror")
-
 ggplot(dh, aes(rating, fill = genregroup)) + 
   geom_histogram(alpha = 0.5, aes(y = ..density..), position = 'identity', bins = 20)+
   ggtitle(paste("avg rating for genregroup (successful movies only)"))
 
-##### performing the welsh t test for significant differences ###
+# performing the welsh t test for significant differences
 shm.test <- t.test(successful_horror$vote_average, successful_non_horror$vote_average, alternative = "less")
-shm.test$p.value
-#this suggests that my suspicion is also true for successful movies 
+cat("t-test:", shm.test$p.value, "\n")
+# again, this suggests that my suspicion is also true for successful movies 
 
-##### comparing american and non american movies ###
+# and now something completely different...
+# compare american and non american movies
 american<-movies[grepl("United States of America", movies$production_countries),]
 non_american<-movies[!grepl("United States of America", movies$production_countries),]
-mean(american$budget)
-mean(non_american$budget)
-mean(american$revenue)
-mean(non_american$revenue)
-mean(american$vote_average)
-mean(non_american$vote_average)
 
 dg <- combine(american$vote_average, non_american$vote_average, "american", "non_american")
 
